fe9d31881b5c1ec21568abd48562677bb1ee1fc4
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2023-09-19 09:28:08 +0300

    examples: Introduce the KServe example
    Add a simple KServe example to test whether KServe works as
    expected.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

---
+++ examples/kserve/sklearn/predict.py
@@ -0,0 +1,20 @@
+import os
+import json
+import requests
+
+
+INGRESS_HOST = os.getenv("INGRESS_HOST", "localhost")
+INGRESS_PORT = os.getenv("INGRESS_PORT", "80")
+URL = f"http://{INGRESS_HOST}:{INGRESS_PORT}/v1/models/sklearn-iris:predict"
+
+
+if __name__ == "__main__":
+    with open("iris-input.json", "r") as f:
+        data = json.load(f)
+
+    headers = {"Host":"sklearn-iris.default.example.com"}
+
+    res = requests.post(URL, json=data, headers=headers)
+
+    print(f"Status code: {res.status_code}")
+    print(f"Predictions: {json.loads(res.text)['predictions']}")
